70cbc63ae84b5b7c6d21fd74f19b7e1c0c03ccf5
jsc-repo <108231974+jsc-repo@users.noreply.github.com>
2022-11-07 10:46:58 -0500

    upgraded to django v4

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,22 +1,12 @@
 from django.db import models
 
 # Create your models here.
-'''class Collection(models.Model):
-    title = models.CharField(max_length = 200)
-    description = models.TextField(max_length = 500)
-
-    def __str__(self):
-        return self.title
-'''
-
-
 class Image(models.Model):
-    #collection = models.ForeignKey(Collection)
-
-    image = models.ImageField(upload_to = 'images')
-    title = models.CharField(max_length = 200)
-    description = models.TextField(max_length = 500, null=False, blank=True)
+    # collection = models.ForeignKey(Collection)
+    id = models.AutoField(primary_key=True)
+    image = models.ImageField(upload_to="images")
+    title = models.CharField(max_length=200)
+    description = models.TextField(max_length=500, null=False, blank=True)
 
     def __str__(self):
         return self.title
-
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,6 +1,6 @@
-from django.conf.urls import url
+from django.urls import re_path as url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.home, name = 'home'),
+    url(r"^$", views.home, name="home"),
 ]
